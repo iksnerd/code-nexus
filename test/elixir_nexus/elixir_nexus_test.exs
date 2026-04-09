baa3d3e8446ca1c99cf5b61a4917f48cafdf76cd
iksnerd <iksnerd@users.noreply.github.com>
2026-04-09 16:00:04 +0300

    Bump version to 0.3.0

--- test/elixir_nexus/elixir_nexus_test.exs
+++ test/elixir_nexus/elixir_nexus_test.exs
@@ -2,6 +2,6 @@ defmodule ElixirNexusTest do
   use ExUnit.Case, async: true
 
   test "version/0 returns current version" do
-    assert ElixirNexus.version() == "0.2.0"
+    assert ElixirNexus.version() == "0.3.0"
   end
 end
